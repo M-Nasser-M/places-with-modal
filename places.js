8628ba8dccd0993a922bf647197abe2e4edaa102
sandspirit2 <momom_2011@hotmail.com>
2019-06-27 04:19:07 +0200

    this is the first commit

---
+++ places.js
@@ -0,0 +1,207 @@
+const places = [
+
+    {
+        name: "place1",
+        clicks: 0,
+        img: "./images/place1.jpg",
+        txt: "cursus suscipit. Nunc scelerisque tortor a nulla tincidunt, quis commodo mi rhoncus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus pulvinar elit quis nisi placerat maximus. Duis fermentum feugiat magna, nec interdum quam elementum id. Nunc elementum ante turpis, vitae varius justo dignissim et. Quisque tincidunt nisl lorem. Duis at augue sit amet erat auctor imperdiet. Integer pharetra massa eu quam posuere, consectetur posuere arcu tristique.. Duis at augue sit amet erat auctor imperdiet. Integer pharetra massa eu quam posuere, consectetur posuere arcu tristique."
+    },
+
+    {
+        name: "place2",
+        clicks: 0,
+        img: "./images/place2.jpg",
+        txt: "cursus suscipit. Nunc scelerisque tortor a nulla tincidunt, quis commodo mi rhoncus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus pulvinar elit quis nisi placerat maximus. Duis fermentum feugiat magna, nec interdum quam elementum id. Nunc elementum ante turpis, vitae varius justo dignissim et. Quisque tincidunt nisl lorem. Duis at augue sit amet erat auctor imperdiet. Integer pharetra massa eu quam posuere, consectetur posuere arcu tristique."
+
+    },
+
+    {
+        name: "place3",
+        clicks: 0,
+        img: "./images/place3.jpg",
+        txt: "cursus suscipit. Nunc scelerisque tortor a nulla tincidunt, quis commodo mi rhoncus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus pulvinar elit quis nisi placerat maximus. Duis fermentum feugiat magna, nec interdum quam elementum id. Nunc elementum ante turpis, vitae varius justo dignissim et. Quisque tincidunt nisl lorem. Duis at augue sit amet erat auctor imperdiet. Integer pharetra massa eu quam posuere, consectetur posuere arcu tristique."
+    },
+
+    {
+        name: "place4",
+        clicks: 0,
+        img: "./images/place4.jpg",
+        txt: "cursus suscipit. Nunc scelerisque tortor a nulla tincidunt, quis commodo mi rhoncus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus pulvinar elit quis nisi placerat maximus. Duis fermentum feugiat magna, nec interdum quam elementum id. Nunc elementum ante turpis, vitae varius justo dignissim et. Quisque tincidunt nisl lorem. Duis at augue sit amet erat auctor imperdiet. Integer pharetra massa eu quam posuere, consectetur posuere arcu tristique."
+    },
+    {
+        name: "place1",
+        clicks: 0,
+        img: "./images/place1.jpg",
+        txt: "cursus suscipit. Nunc scelerisque tortor a nulla tincidunt, quis commodo mi rhoncus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus pulvinar elit quis nisi placerat maximus. Duis fermentum feugiat magna, nec interdum quam elementum id. Nunc elementum ante turpis, vitae varius justo dignissim et. Quisque tincidunt nisl lorem. Duis at augue sit amet erat auctor imperdiet. Integer pharetra massa eu quam posuere, consectetur posuere arcu tristique."
+
+    },
+
+    {
+        name: "place2",
+        clicks: 0,
+        img: "./images/place2.jpg",
+        txt: "cursus suscipit. Nunc scelerisque tortor a nulla tincidunt, quis commodo mi rhoncus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus pulvinar elit quis nisi placerat maximus. Duis fermentum feugiat magna, nec interdum quam elementum id. Nunc elementum ante turpis, vitae varius justo dignissim et. Quisque tincidunt nisl lorem. Duis at augue sit amet erat auctor imperdiet. Integer pharetra massa eu quam posuere, consectetur posuere arcu tristique."
+    },
+
+    {
+        name: "place3",
+        clicks: 0,
+        img: "./images/place3.jpg",
+        txt: "cursus suscipit. Nunc scelerisque tortor a nulla tincidunt, quis commodo mi rhoncus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus pulvinar elit quis nisi placerat maximus. Duis fermentum feugiat magna, nec interdum quam elementum id. Nunc elementum ante turpis, vitae varius justo dignissim et. Quisque tincidunt nisl lorem. Duis at augue sit amet erat auctor imperdiet. Integer pharetra massa eu quam posuere, consectetur posuere arcu tristique."
+    },
+
+    {
+        name: "place4",
+        clicks: 0,
+        img: "./images/place4.jpg",
+        txt: "cursus suscipit. Nunc scelerisque tortor a nulla tincidunt, quis commodo mi rhoncus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus pulvinar elit quis nisi placerat maximus. Duis fermentum feugiat magna, nec interdum quam elementum id. Nunc elementum ante turpis, vitae varius justo dignissim et. Quisque tincidunt nisl lorem. Duis at augue sit amet erat auctor imperdiet. Integer pharetra massa eu quam posuere, consectetur posuere arcu tristique."
+    },
+    {
+        name: "place1",
+        clicks: 0,
+        img: "./images/place1.jpg",
+        txt: "cursus suscipit. Nunc scelerisque tortor a nulla tincidunt, quis commodo mi rhoncus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus pulvinar elit quis nisi placerat maximus. Duis fermentum feugiat magna, nec interdum quam elementum id. Nunc elementum ante turpis, vitae varius justo dignissim et. Quisque tincidunt nisl lorem. Duis at augue sit amet erat auctor imperdiet. Integer pharetra massa eu quam posuere, consectetur posuere arcu tristique."
+    },
+
+    {
+        name: "place2",
+        clicks: 0,
+        img: "./images/place2.jpg",
+        txt: "cursus suscipit. Nunc scelerisque tortor a nulla tincidunt, quis commodo mi rhoncus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus pulvinar elit quis nisi placerat maximus. Duis fermentum feugiat magna, nec interdum quam elementum id. Nunc elementum ante turpis, vitae varius justo dignissim et. Quisque tincidunt nisl lorem. Duis at augue sit amet erat auctor imperdiet. Integer pharetra massa eu quam posuere, consectetur posuere arcu tristique."
+    },
+
+    {
+        name: "place3",
+        clicks: 0,
+        img: "./images/place3.jpg",
+        txt: "cursus suscipit. Nunc scelerisque tortor a nulla tincidunt, quis commodo mi rhoncus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus pulvinar elit quis nisi placerat maximus. Duis fermentum feugiat magna, nec interdum quam elementum id. Nunc elementum ante turpis, vitae varius justo dignissim et. Quisque tincidunt nisl lorem. Duis at augue sit amet erat auctor imperdiet. Integer pharetra massa eu quam posuere, consectetur posuere arcu tristique."
+    },
+
+    {
+        name: "place4",
+        clicks: 0,
+        img: "./images/place4.jpg",
+        txt: "cursus suscipit. Nunc scelerisque tortor a nulla tincidunt, quis commodo mi rhoncus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus pulvinar elit quis nisi placerat maximus. Duis fermentum feugiat magna, nec interdum quam elementum id. Nunc elementum ante turpis, vitae varius justo dignissim et. Quisque tincidunt nisl lorem. Duis at augue sit amet erat auctor imperdiet. Integer pharetra massa eu quam posuere, consectetur posuere arcu tristique."
+    }, {
+        name: "place1",
+        clicks: 0,
+        img: "./images/place1.jpg",
+        txt: "cursus suscipit. Nunc scelerisque tortor a nulla tincidunt, quis commodo mi rhoncus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus pulvinar elit quis nisi placerat maximus. Duis fermentum feugiat magna, nec interdum quam elementum id. Nunc elementum ante turpis, vitae varius justo dignissim et. Quisque tincidunt nisl lorem. Duis at augue sit amet erat auctor imperdiet. Integer pharetra massa eu quam posuere, consectetur posuere arcu tristique."
+    },
+
+    {
+        name: "place2",
+        clicks: 0,
+        img: "./images/place2.jpg",
+        txt: "cursus suscipit. Nunc scelerisque tortor a nulla tincidunt, quis commodo mi rhoncus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus pulvinar elit quis nisi placerat maximus. Duis fermentum feugiat magna, nec interdum quam elementum id. Nunc elementum ante turpis, vitae varius justo dignissim et. Quisque tincidunt nisl lorem. Duis at augue sit amet erat auctor imperdiet. Integer pharetra massa eu quam posuere, consectetur posuere arcu tristique."
+    },
+
+    {
+        name: "place3",
+        clicks: 0,
+        img: "./images/place3.jpg",
+        txt: "cursus suscipit. Nunc scelerisque tortor a nulla tincidunt, quis commodo mi rhoncus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus pulvinar elit quis nisi placerat maximus. Duis fermentum feugiat magna, nec interdum quam elementum id. Nunc elementum ante turpis, vitae varius justo dignissim et. Quisque tincidunt nisl lorem. Duis at augue sit amet erat auctor imperdiet. Integer pharetra massa eu quam posuere, consectetur posuere arcu tristique."
+    },
+
+    {
+        name: "place4",
+        clicks: 0,
+        img: "./images/place4.jpg",
+        txt: "cursus suscipit. Nunc scelerisque tortor a nulla tincidunt, quis commodo mi rhoncus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus pulvinar elit quis nisi placerat maximus. Duis fermentum feugiat magna, nec interdum quam elementum id. Nunc elementum ante turpis, vitae varius justo dignissim et. Quisque tincidunt nisl lorem. Duis at augue sit amet erat auctor imperdiet. Integer pharetra massa eu quam posuere, consectetur posuere arcu tristique."
+    },
+
+
+];
+
+
+
+// for (let i = 0; i < avengers.length / 4; i++) {
+
+//     let card_deck = document.createElement("div");
+//     card_deck.className += " card-deck"
+
+//     for (let j = 4 * i; j < 4 * i + 4; j++) {
+
+//         var card = document.createElement("div");
+//         card.className += " card";
+//         var card_img = document.createElement("img");
+//         card_img.className += " card-img-top img-fluid";
+//         card_img.setAttribute("src", avengers[j].img);
+//         card.append(card_img);
+//         var card_body = document.createElement("div");
+//         card_body.className += " card-body";
+//         var card_title = document.createElement("h4");
+//         card_title.className += " card-title";
+//         card_title.innerText = avengers[j].name;
+//         card_body.append(card_title);
+//         var card_text = document.createElement("p");
+//         card_text.className += " card-text";
+//         card_body.append(card_text);
+//         card.append(card_body);
+//         card_deck.append(card);
+
+//     }
+//     $("#places").append(card_deck);
+
+// }
+
+$('#places').on('click', '.card', sohwmodal);
+
+for (let i = 0; i < places.length; i++) {
+
+    let card_deck = document.createElement("div");
+    card_deck.className += " col-12 col-md-3  my-3 "
+
+
+
+    var card = document.createElement("div");
+    card.className += " card text-center";
+    card.setAttribute("data-toggle", "modal");
+    card.setAttribute("data-target", "#exampleModalScrollable");
+
+    var card_img = document.createElement("img");
+    card_img.className += " card-img-top img-fluid img-h";
+    card_img.setAttribute("src", places[i].img);
+    card.append(card_img);
+
+    var card_body = document.createElement("div");
+    card_body.className += " card-body";
+
+    var card_title = document.createElement("h4");
+    card_title.className += " card-title";
+    card_title.innerText = places[i].name;
+    card_body.append(card_title);
+
+    var card_text = document.createElement("p");
+    card_text.className += " card-text";
+    card_text.innerText = places[i].txt;
+    card_body.append(card_text);
+
+    var card_footer = document.createElement("div");
+    card_footer.className += "card-footer";
+
+    var foot_text1 = document.createElement("small");
+    foot_text1.className += "mr-4"
+    foot_text1.innerHTML = '<i class="fas fa-calendar mr-1   "></i>aug5,2013';
+    card_footer.append(foot_text1);
+
+    var foot_text2 = document.createElement("small");
+    foot_text2.className += "ml-4"
+    foot_text2.innerHTML = '<i class="fas fa-comment mr-1   "></i>comments';
+    card_footer.append(foot_text2);
+
+    card.append(card_body);
+    card.append(card_footer);
+    card_deck.append(card);
+
+
+    $("#places").append(card_deck);
+}
+
+function sohwmodal() {
+    var modal_card = $(this).clone();
+    console.log($(this).children(".card-body").children(".card-title").text());
+    $(".modal-title").text($(this).children(".card-body").children(".card-title").text());
+    $(".modal-body").children().remove();
+    $(".modal-body").append(modal_card);
+}
